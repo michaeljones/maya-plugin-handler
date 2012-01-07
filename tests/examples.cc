1a74a1b9ade19cc72c2c373fbb0a7618ab2ad967
Michael Jones <mjones@wetafx.co.nz>
2012-01-08 08:00:45 +1300

    Fixing conflicts between example names and MTypeIds
    Copy & paste fail.

--- tests/examples.cc
+++ tests/examples.cc
@@ -1,15 +1,15 @@
 
 #include "examples.hh"
 
-MTypeId Locator::id( 0x0, 0x0 );
+MTypeId Locator::id( 0x8001 );
 
-MTypeId FullOpinionatedLocator::id( 0x0, 0x0 );
+MTypeId FullOpinionatedLocator::id( 0x80001 );
 MString FullOpinionatedLocator::nodeName( "fullOpinionatedLocator" );
 MPxNode::Type FullOpinionatedLocator::nodeType( MPxNode::kLocatorNode );
 MString FullOpinionatedLocator::classification( "customClassification" );
 
-MTypeId PartialOpinionatedNode::id( 0x0, 0x0 );
+MTypeId PartialOpinionatedNode::id( 0x80002 );
 MString PartialOpinionatedNode::nodeName( "partialOpinionatedNode" );
 
-MString Command::commandName( "commandExample" );
+MString Command::commandName( "opinionatedCommand" );
 
